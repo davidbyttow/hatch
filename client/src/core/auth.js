bf33295c8f96792e55a642b0665d43b043a9986f
davidbyttow <david.byttow@gmail.com>
2017-05-26 17:26:30 -0700

    Removes logging

--- client/src/core/auth.js
+++ client/src/core/auth.js
@@ -5,16 +5,13 @@ const provider = new firebase.auth.TwitterAuthProvider();
 
 export const signInWithPopup = () => {
   return firebase.auth().signInWithPopup(provider).then((result) => {
-    console.log('success', result);
     const { accessToken, secret } = result.credential;
     const { user } = result;
     return user;
   }).catch((error) => {
-    console.log('error', error);
     throw error;
   })
 };
 
 firebase.auth().onAuthStateChanged(function(user) {
-  console.log('user', user)
 });
